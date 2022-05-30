03fd08a5f0c42c10952c356767668eca30f16bc0
Alex-OpenOrigins <97303133+Alex-OpenOrigins@users.noreply.github.com>
2022-05-30 14:02:43 +0100

    Add build files

---
+++ build/index.d.ts
@@ -0,0 +1,46 @@
+import { StrategyVerifyCallback } from "remix-auth";
+import { OAuth2Profile, OAuth2Strategy, OAuth2StrategyVerifyParams } from "remix-auth-oauth2";
+export interface MicrosoftStrategyOptions {
+    clientID: string;
+    clientSecret: string;
+    callbackURL: string;
+    scope?: string;
+    tenant?: string;
+    prompt?: string;
+    baseURL?: string;
+    userFlowID?: string;
+}
+export interface MicrosoftProfile extends OAuth2Profile {
+    id: string;
+    displayName: string;
+    name: {
+        familyName: string;
+        givenName: string;
+    };
+    emails: [{
+        value: string;
+    }];
+    _json: {
+        sub: string;
+        name: string;
+        family_name: string;
+        given_name: string;
+        email: string;
+    };
+}
+export interface MicrosoftExtraParams extends Record<string, string | number> {
+    expires_in: 3599;
+    token_type: "Bearer";
+    scope: string;
+    id_token: string;
+}
+export declare class MicrosoftStrategy<User> extends OAuth2Strategy<User, MicrosoftProfile, MicrosoftExtraParams> {
+    name: string;
+    private scope;
+    private prompt;
+    private userFlowID;
+    private userInfoURL;
+    constructor({ clientID, clientSecret, callbackURL, scope, prompt, tenant, baseURL, userFlowID, }: MicrosoftStrategyOptions, verify: StrategyVerifyCallback<User, OAuth2StrategyVerifyParams<MicrosoftProfile, MicrosoftExtraParams>>);
+    protected authorizationParams(): URLSearchParams;
+    protected userProfile(accessToken: string): Promise<MicrosoftProfile>;
+}
---
+++ build/index.js
@@ -0,0 +1,48 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MicrosoftStrategy = void 0;
+const remix_auth_oauth2_1 = require("remix-auth-oauth2");
+class MicrosoftStrategy extends remix_auth_oauth2_1.OAuth2Strategy {
+    constructor({ clientID, clientSecret, callbackURL, scope, prompt, tenant = "common", baseURL = "login.microsoftonline.com", userFlowID, }, verify) {
+        super({
+            clientID,
+            clientSecret,
+            callbackURL,
+            authorizationURL: `https://${baseURL}/${tenant}/oauth2/v2.0/authorize`,
+            tokenURL: `https://${baseURL}/${tenant}/oauth2/v2.0/token`,
+        }, verify);
+        this.name = "microsoft";
+        this.userInfoURL = "https://graph.microsoft.com/oidc/userinfo";
+        this.scope = scope !== null && scope !== void 0 ? scope : "openid profile email";
+        this.prompt = prompt !== null && prompt !== void 0 ? prompt : "none";
+        this.userFlowID = userFlowID !== null && userFlowID !== void 0 ? userFlowID : "";
+    }
+    authorizationParams() {
+        return new URLSearchParams({
+            scope: this.scope,
+            prompt: this.prompt,
+            p: this.userFlowID
+        });
+    }
+    async userProfile(accessToken) {
+        let response = await fetch(this.userInfoURL, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+        let data = await response.json();
+        let profile = {
+            provider: "microsoft",
+            displayName: data.name,
+            id: data.sub,
+            name: {
+                familyName: data.family_name,
+                givenName: data.given_name,
+            },
+            emails: [{ value: data.email }],
+            _json: data,
+        };
+        return profile;
+    }
+}
+exports.MicrosoftStrategy = MicrosoftStrategy;
